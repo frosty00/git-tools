f344b5d133ab29fa49a0a14be1140a61f0866535
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-10-21 03:37:28 -0700

    refix order of operations

--- autopush.sh
+++ autopush.sh
@@ -12,11 +12,11 @@ if [ -n "$exists" ]; then
 else
   git checkout -b "$id"
 fi
-git add -u
 if [[ $? != 0 ]]; then
   echo "invalid branch name: $1"
   exit 1
 fi
+git add -u
 git commit -m "$id"
 git push github
 git checkout master
