16e9475a82b3db736e01b52228eb1136437f9a92
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-12-29 07:50:34 -0800

    now support auto creating a pull request

--- autopush.sh
+++ autopush.sh
@@ -19,3 +19,27 @@ fi
 git commit -am "$id"
 git push -u github
 git checkout master
+
+origin=$(git remote -v | grep origin | head -n 1 | awk '{ print $2 }')
+
+if [[ $origin =~ ^https ]]; then
+  owner=$(cut -d / -f4 <<< $origin)
+  repo=$(cut -d / -f5 <<< $origin)
+elif [[ $origin =~ ^git\@github\.com ]]; then
+  path=$(cut -d : -f2 <<< $origin)
+  owner=$(cut -d / -f1 <<< $path)
+  repo=$(cut -d / -f2 <<< $path)
+else
+  echo "invalid origin repo - $origin, please add an origin remote branch tracking a git repo" >&2
+  exit 4
+fi
+
+body=$(jq --null-input --arg title "$id" --arg head "frosty00:$id" --arg base "master" '{ $title, $head, $base }')
+echo "$body"
+curl \
+  -X POST \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer $GITHUB_AUTH_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/$owner/$repo/pulls" \
+  -d "$body"
